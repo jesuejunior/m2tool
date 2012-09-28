c908e6cebe6bb0d5a2eee43ccc472a0fba1c4bb7
JesueJunior <jesuesousa@gmail.com>
2012-09-28 19:38:13 -0300

    Preparando para receber varios parametors para qualquer função

--- m2tool/commands/server.py
+++ m2tool/commands/server.py
@@ -1,2 +1,6 @@
-def server_command():
+def server_command(name, port):
+    '''
+    
+    '''
     print "command_server"
+    print "Adicionando servidor {0} porta {1}".format(name, port)
--- m2tool/main.py
+++ m2tool/main.py
@@ -7,12 +7,13 @@ def main():
     command, params = args.parse(sys.argv[1:])
     all_commands = args.available_commands(m2tool.commands)
 
+    print params
     if command not in all_commands:
         print "Command {0} not found!".format(command)
 
     else:
         print "Running {0}".format(command)
         f = args.function_for(m2tool.commands, command)
-        f()
+        f(**params)
 
     sys.exit(0)
