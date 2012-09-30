929d008abb4f7eb01df0028953a59fa7e0a8370e
Dalton Barreto <daltonmatos@gmail.com>
2012-09-30 20:38:10 -0300

    If no envvar, defaults to pwd()/config.sqlite

--- m2tool/db/__init__.py
+++ m2tool/db/__init__.py
@@ -4,9 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import MetaData
 from sqlalchemy import create_engine
 
-from m2tool.conf import M2_DB_ENV, M2_DEFAULT_DB_NAME
+from m2tool.conf import M2_DB_ENV, M2_DEFAULT_DB_PATH
 
-engine = create_engine("sqlite+pysqlite:///{0}".format(os.environ[M2_DB_ENV]))
+engine = create_engine("sqlite+pysqlite:///{0}".format(os.environ.get(M2_DB_ENV, M2_DEFAULT_DB_PATH)))
 Metadata = MetaData(bind=engine)
 Model = declarative_base(metadata=Metadata)
 Session = sessionmaker(bind=engine)
