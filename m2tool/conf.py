0a0b08bd3dd722c9d8043e0ac7d0f82ee52b7563
Dalton Barreto <daltonmatos@gmail.com>
2012-09-30 16:18:12 -0300

    Option to inform which m2db to use
    You can pass --m2db or create and environ variable named M2_DB containing the complete path to your mongrel2 database file

--- m2tool/conf.py
+++ m2tool/conf.py
@@ -3,3 +3,8 @@ import os
 
 DIRNAME = '.m2tool'
 CFGDIR = os.path.join(os.path.expanduser('~'), DIRNAME)
+
+M2_DB_ENV = 'M2_DB'
+M2_DEFAULT_DB_NAME = 'config.sqlite'
+
+M2_DEFAULT_DB_PATH = os.path.join(os.getcwd(), M2_DEFAULT_DB_NAME)
--- m2tool/main.py
+++ m2tool/main.py
@@ -1,11 +1,17 @@
 from modargs import args
 import sys
-import m2tool.commands
+import os
 
+import m2tool.commands
+from m2tool.conf import M2_DB_ENV, M2_DEFAULT_DB_PATH
 
 def main():
     command, params = args.parse(sys.argv[1:])
     all_commands = args.available_commands(m2tool.commands)
+    print M2_DB_ENV
+    os.environ.setdefault(M2_DB_ENV, params.get('m2db', M2_DEFAULT_DB_PATH))
+
+    print "Using DB {0}".format(os.environ.get(M2_DB_ENV))
 
     print params
     if command not in all_commands:
