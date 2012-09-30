7929dd9f44cdf00f78226ead2d972a24a3c96921
JesueJunior <jesuesousa@gmail.com>
2012-09-30 20:38:49 -0300

    Start mapper model of server

--- m2tool/db/models.py
+++ m2tool/db/models.py
@@ -1,1 +1,18 @@
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer
+import m2tool
 
+class Server(m2tool.db.Model):
+    __tablename__ = 'servers'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    port = Column(String)
+    uuid = Column(String)
+    accesslog = Column(String)
+    errorlog = Column(String)
+    chroot = Column(String)
+    pidfile = Column(String)
+    bindaddr = Column(String)
+    usessl = Column(String)
+    defaulthost = Column(String)
