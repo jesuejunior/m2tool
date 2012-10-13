2e28543b3af16db8e75e587b11e28b09226a509c
JesueJunior <jesuesousa@gmail.com>
2012-10-13 01:54:53 -0300

    Commands ADD and REMOVE

--- m2tool/commands/server.py
+++ m2tool/commands/server.py
@@ -1,20 +1,16 @@
 #coding: utf-8
-from sys import argv
 from uuid import uuid4
-import sys
 from m2tool.db import Session
 from m2tool.db.models import Server
 
-def server_command(function,name, port, chroot=None, bindaddr=None, pidfile=None,
-                   defaulthost=None, accesslog=None, errorlog=None, ssl=False,uuid=None):
+def server_command(function, name=None, port=None, chroot=None, bindaddr=None, pidfile=None,
+                   defaulthost=None, accesslog=None, errorlog=None, ssl=False, uuid=None, id=None):
     '''
     --uuid=UUID --accesslog=FILE --errorlog=FILE
     --chroot=PATH --pidfile=FILE --name=NAME
     --bindaddr=IP --port=PORT --ssl
     --defaulthost=HOSTNAME
     '''
-
-    print function
 
     if not uuid:
         uuid = str(uuid4())
@@ -27,7 +23,7 @@ def server_command(function,name, port, chroot=None, bindaddr=None, pidfile=None
         bindaddr = '0.0.0.0'
 
     if not pidfile:
-        pidfile = "pid"
+        pidfile = "/run/mongrel2.pid"
 
     if not accesslog:
         accesslog = '/logs/access.log'
@@ -41,18 +37,35 @@ def server_command(function,name, port, chroot=None, bindaddr=None, pidfile=None
         usessl = ssl
 
     session = Session()
-
     port_verify = session.query(Server).filter_by(port=port).count()
     uuid_verify = session.query(Server).filter_by(uuid=uuid).count()
 
     if port_verify :
         print 'Porta [{0}] já está em uso, por favor tente outra'.format(port)
     elif uuid_verify:
-        print 'UUID [{0}] já existe, por favor verifique'.format(uuid)
+        print 'UUID [{0}] already exists, please check.'.format(uuid)
+
+    if function == 'add':
+        if not port or not name:
+            print "Please verify parameters --port or --name"
+        else:
+            server = Server(name=name, port=port, chroot=chroot,  bind_addr=bindaddr, pid_File=pidfile,
+                default_host=defaulthost, access_log=accesslog, error_log=errorlog, use_ssl=usessl, uuid=uuid )
+
+            session.add(server)
+            session.commit()
+            print 'Congratulations! Server [{0}] adding with success.'.format(name)
+
+    elif function == 'remove':
+        server = session.query(Server).get(id)
+        if server:
+            session.query(Server).filter_by(id=id).delete()
+            session.commit()
+            print 'Server [{0}] was removed with success.'.format(server.name)
+        else:
+            print 'Server not found.'
+    elif function == 'update':
+        print 'up'
 
     else:
-        server = Server(name=name, port=port, chroot=chroot,  bind_addr=bindaddr, pid_File=pidfile,
-            default_host=defaulthost, access_log=accesslog, error_log=errorlog, use_ssl=usessl, uuid=uuid )
-
-        session.add(server)
-        session.commit()
+        print 'Any option selected.'
