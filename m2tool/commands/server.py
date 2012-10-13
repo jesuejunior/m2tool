b30a02eba7449926985da35892b35b73abc4bcd4
JesueJunior <jesuesousa@gmail.com>
2012-10-13 02:40:20 -0300

    Adding command basic server list

--- m2tool/commands/server.py
+++ m2tool/commands/server.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from m2tool.db import Session
 from m2tool.db.models import Server
 
-def server_command(function, name=None, port=None, chroot=None, bindaddr=None, pidfile=None,
+def server_command(option, name=None, port=None, chroot=None, bindaddr=None, pidfile=None,
                    defaulthost=None, accesslog=None, errorlog=None, ssl=False, uuid=None, id=None):
     '''
     --uuid=UUID --accesslog=FILE --errorlog=FILE
@@ -45,27 +45,32 @@ def server_command(function, name=None, port=None, chroot=None, bindaddr=None, p
     elif uuid_verify:
         print 'UUID [{0}] already exists, please check.'.format(uuid)
 
-    if function == 'add':
-        if not port or not name:
-            print "Please verify parameters --port or --name"
-        else:
-            server = Server(name=name, port=port, chroot=chroot,  bind_addr=bindaddr, pid_File=pidfile,
-                default_host=defaulthost, access_log=accesslog, error_log=errorlog, use_ssl=usessl, uuid=uuid )
-
-            session.add(server)
-            session.commit()
-            print 'Congratulations! Server [{0}] adding with success.'.format(name)
-
-    elif function == 'remove':
-        server = session.query(Server).get(id)
-        if server:
-            session.query(Server).filter_by(id=id).delete()
-            session.commit()
-            print 'Server [{0}] was removed with success.'.format(server.name)
-        else:
-            print 'Server not found.'
-    elif function == 'update':
-        print 'up'
-
     else:
-        print 'Any option selected.'
+        if option == 'add':
+            if not port or not name:
+                print "Please verify parameters --port or --name"
+            else:
+                server = Server(name=name, port=port, chroot=chroot,  bind_addr=bindaddr, pid_File=pidfile,
+                    default_host=defaulthost, access_log=accesslog, error_log=errorlog, use_ssl=usessl, uuid=uuid )
+
+                session.add(server)
+                session.commit()
+                print 'Congratulations! Server [{0}] adding with success.'.format(name)
+
+        elif option == 'remove':
+            server = session.query(Server).get(id)
+            if server:
+                session.query(Server).filter_by(id=id).delete()
+                session.commit()
+                print 'Server [{0}] was removed with success.'.format(server.name)
+            else:
+                print 'Server not found.'
+        elif option == 'update':
+            print 'up'
+        elif option == 'list':
+            server = session.query(Server).order_by(Server.id).all()
+            print  '| ' + 'id' + '  |  ' + 'Name' + '  |  ' + 'Port' + ' | '
+            for servers in server:
+                print servers.id , servers.name, servers.port
+        else:
+            print 'Any option selected.'
