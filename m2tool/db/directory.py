50fcd54de37b70c1b4b929fd288939bf62862849
Jesue Junior <jesuesousa@gmail.com>
2012-12-22 15:49:52 -0200

    Finalizando modelo de directory

--- m2tool/db/directory.py
+++ m2tool/db/directory.py
@@ -1,1 +1,21 @@
-__author__ = 'jesuejunior'
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, String, Boolean
+import m2tool
+
+class Directory(m2tool.db.Model):
+    __tablename__ = 'directory'
+
+    id = Column(Integer, primary_key=True)
+    base = Column(String, nullable=False)
+    index_file = Column(String, nullable=False)
+    default_ctype = Column(String, nullable=False)
+    cache_ttl = Column(Boolean, default=0)
+
+    def __init__(self, base, index_file, default_ctype, cache_ttl):
+        self.base = base
+        self.index_file = index_file
+        self.default_ctype = default_ctype
+        self.cache_ttl = cache_ttl
+
+
+
