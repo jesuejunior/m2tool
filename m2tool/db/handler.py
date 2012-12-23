0500653a7b2cb86beb6a242778a813273f2fb516
Jesue Junior <jesuesousa@gmail.com>
2012-12-23 12:20:23 -0200

    Adicionando modelo handler

--- m2tool/db/handler.py
+++ m2tool/db/handler.py
@@ -1,1 +1,23 @@
-__author__ = 'jesuejunior'
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, String, Boolean
+import m2tool
+
+class Handler(m2tool.db.Model):
+    __tablename__ = 'handler'
+
+    id = Column(Integer, primary_key=True)
+    send_spec = Column(String, nullable=False)
+    send_ident = Column(String, nullable=False)
+    recv_spec = Column(String, nullable=False)
+    recv_ident = Column(String, nullable=False)
+    raw_payload = Column(Integer, default=1)
+    protocol = Column(String, default='json')
+
+
+    def __init__(self, send_spec, send_ident, recv_spec, recv_ident, raw_payload, protocol):
+        self.send_spec = send_spec
+        self.send_ident = send_ident
+        self.recv_spec = recv_spec
+        self.recv_ident = recv_ident
+        self.raw_payload = raw_payload
+        self.protocol = protocol
