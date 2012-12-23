b397f9f02eb78076ec52a8520979d2f9670823c4
Jesue Junior <jesuesousa@gmail.com>
2012-12-23 12:58:46 -0200

    Adicionando modelo do Host

--- m2tool/db/host.py
+++ m2tool/db/host.py
@@ -1,1 +1,18 @@
-__author__ = 'jesuejunior'
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer,String,Boolean
+
+import m2tool
+
+
+class Host(m2tool.db.Model):
+    id = Column(Integer, primary_ket=True)
+    server_id = Column(Integer, nullable=False)
+    maintenance = Column(Boolean, default=0)
+    name = Column(String(50), nullable=False)
+    matching = Column(String, nullable=False)
+
+    def __init__(self, server_id, maintenance, name, matching):
+        self.server_id = server_id
+        self.maintenance = maintenance
+        self.name = name
+        self.matching = matching
