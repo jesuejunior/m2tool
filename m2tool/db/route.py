e5bf153f3e826c502a47521f845eb945e59a8bb3
Jesue Junior <jesuesousa@gmail.com>
2012-12-23 13:26:56 -0200

    Adicionando modelo do route

--- m2tool/db/route.py
+++ m2tool/db/route.py
@@ -1,1 +1,20 @@
-__author__ = 'jesuejunior'
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer,String,Boolean
+
+import m2tool
+
+
+class Route(m2tool.db.Model):
+    id = Column(Integer, primary_key=True)
+    path = Column(String, nullable=False)
+    reversed = Column(Boolean, default=0)
+    host_id = Column(Integer, nullable=False)
+    target_id = Column(Integer, nullable=False)
+    target_type = Column(String(10), nullable=False)
+
+    def __init__(self, path, reversed, host_id, target_id, target_type):
+        self.path = path
+        self.reversed = reversed
+        self.host_id = host_id
+        self.target_id = target_id
+        self.target_type = target_type
